86076be4ff479cd5943eb4d6d4ab4e93d55638b3
TejasVarute <tvarute@gmail.com>
2025-05-19 19:44:07 +0530

    add loader

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,15 @@
+window.addEventListener("load", function () {
+  document.getElementById("main").style.display = "none";
+  const loader = document.getElementById("loader");
+  loader.style.display = "block";
+
+  setTimeout(() => {
+    loader.style.display = "none";
+    document.getElementById("main").style.display = "block";
+  }, 2000);
+});
+
 window.addEventListener("scroll", function () {
-    var header = document.querySelector("nav");
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
+  var header = document.querySelector("nav");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
